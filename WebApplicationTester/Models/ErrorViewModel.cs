a54462cb51befbf47aa8c407c148c10c7e30cfb3
JuliaGrisales <jgrisales@gmail.com>
2020-04-07 13:23:31 +0200

    Estado inicial de la APP

---
+++ WebApplicationTester/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationTester.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
